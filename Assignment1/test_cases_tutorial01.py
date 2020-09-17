c224d66dee0c2aee85b1a2beb02d920f4414bdfc
utsavmajhi <utsavmajhi@gmail.com>
2020-09-17 12:13:23 +0530

    implemented the Power function with valid check cases

--- Assignment1/test_cases_tutorial01.py
+++ Assignment1/test_cases_tutorial01.py
@@ -1,6 +1,6 @@
 import tutorial01 as A1
 
-actual_answers = [9, 12, 28, 7]
+actual_answers = [9, 12, 28, 7,4]
 student_answers = []
 
 test_case_1 = A1.add(4, 5)
@@ -14,6 +14,12 @@ student_answers.append(test_case_3)
 
 test_case_4 = A1.divide(14, 2)
 student_answers.append(test_case_4)
+
+test_case_5 = A1.power(2, 2)
+student_answers.append(test_case_5)
+
+test_case_6 = A1.printGP(2,3,5)
+student_answers.append(test_case_6)
 
 print(actual_answers)
 print(student_answers)
--- Assignment1/tutorial01.py
+++ Assignment1/tutorial01.py
@@ -18,8 +18,46 @@ def multiply(num1, num2):
 def divide(num1, num2): 
 	#DivisionLogic
     if (num2==0):
-	    print("Can't divide by 0")
-	    return "Error"
+	    return 0
     division = num1/num2
     return division
+
+# Function to add power function
+#You cant use the inbuilt python function x ** y . Write your own function
+def power(num1, num2): #num1 ^ num2
+	#DivisionLogic
+	power=1
+	if(isinstance(num1,(int, float)) and isinstance(num2,(int, float))):
+		#go ahead
+		if(num2<0):
+			for i in range(abs(int(num2))):
+				power=power*divide(1,num1)
+		else:
+			for i in range(abs(int(num2))):
+				power=power*num1
+	else:
+		return 0
+	if(isinstance(num1,(float))):
+		return round(power,3)
+	else:
+		return power
 	
+# Python 3 program to print GP.  geometric Progression
+#You cant use the inbuilt python function. Write your own function
+def printGP(a, r, n): 
+	gp=[]
+	return gp 
+
+# Python 3 program to print AP.  arithmetic Progression
+#You cant use the inbuilt python function. Write your own function
+def printAP(a, d, n): 
+	ap=[]
+
+	return ap
+
+# Python 3 program to print HP.   Harmonic Progression
+#You cant use the inbuilt python function. Write your own function
+def printHP(a, d, n): 
+	hp=[]
+	return hp
+
